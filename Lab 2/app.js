a56e3643400cc6d73723f6dc9208ec40c34a2cae
OlehLC <o.lobko2018@gmail.com>
2025-02-11 15:57:16 +0200

    Completed Lab2

--- Lab 2/app.js
+++ Lab 2/app.js
@@ -9,6 +9,20 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/student/:id', (req, res) => {
+    const studentId = req.params.id;
+    const students = [
+      { id: 1, name: 'Олег', role: 'Тимлід' },
+      { id: 2, name: 'Микита', role: 'Розробник' },
+      { id: 3, name: 'Катерина', role: 'Дизайнер' }
+    ]; 
+    const student = students.find(s => s.id == studentId);
+    if (!student) {
+      return res.status(404).send('Студента не знайдено');
+    }
+  
+    res.render('student', { student });
+  });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
